c27747d5c46f665669b9713d72a2c7aacb2aac44
Scrattlebeard <juhl.daniel@gmail.com>
2022-11-27 11:22:02 +0100

    Update and simplify build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,5 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.7.10"
-    application
+    kotlin("jvm") version "1.7.21"
 }
 
 group = "org.netcompany.unions.adventofcode"
@@ -10,20 +7,4 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-}
-
-dependencies {
-    testImplementation(kotlin("test"))
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
-}
-
-application {
-    mainClass.set("MainKt")
 }
